2e78ed27d487624288b9e7627256c927859fa82c
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-05-27 08:26:32 -0400

    Use non-deprecated macro and definitions for setting up multiplayer

--- RescueEscortMultiplayer/LevelMainMultiplayer.cpp
+++ RescueEscortMultiplayer/LevelMainMultiplayer.cpp
@@ -3,22 +3,7 @@
 #include <HFL\Source\HFL.h>
 #include <Outpost2DLL/Outpost2DLL.h>
 
-// Required data exports  (Description, Map, TechTree, GameType, NumPlayers)
-ExportLevelDetailsEx("2P, Convoy, 'Rescue Escort'", "RescueEscort.map", "PursuedTechTree.txt", MissionTypes::MultiLandRush, 2, 12, true);
-
-// Allows an enemy AI in a multiplayer scenario.
-struct SDescBlockEx {
-	int unk0;
-	int unk1;
-	int unk2;
-	int unk3;
-	int unk4;
-	int unk5;
-	int unk6;
-	int unk7;
-};
-
-SCRIPT_API SDescBlockEx DescBlockEx = { 1, 0, 0, 0, 0, 0, 0, 0 };
+ExportLevelDetailsFullEx("2P, Convoy, 'Rescue Escort'", "RescueEscort.map", "PursuedTechTree.txt", MultiLandRush, 2, 12, true, 1);
 
 //extern void ShowBriefing();
 
