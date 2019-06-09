5b8cff2b5af4320581c5cabdde650171e3fcc5a5
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-06-08 20:26:42 -0400

    Clean up spacing

--- Briefing.h
+++ Briefing.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-
 #include "resource.h"
 #include "odasl/odasl.h"
 #include <richedit.h>
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 void ShowBriefing();
